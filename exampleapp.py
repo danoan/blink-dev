4efa7181fadb2ed4f78f22b176da937e117fe1d6
daniel <danoan2008@gmail.com>
2013-08-30 10:51:36 -0300

    International Blinker implemented; Code Refactoring; First Test Routine Implemented;

--- exampleapp.py
+++ exampleapp.py
@@ -10,7 +10,7 @@ import hashlib
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 
 import requests
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, session, request, redirect, render_template, url_for
 
 FB_APP_ID = os.environ.get('FACEBOOK_APP_ID')
 requests = requests.session()
@@ -126,7 +126,7 @@ def fb_call(call, args=None):
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_object('conf.Config')
-
+app.secret_key = "d5X2=!T774/=$z#(c@K3"
 
 def get_home():
     return 'https://' + request.host + '/'
@@ -177,11 +177,132 @@ import datetime
 paths_dict = {"CSS_PATH":"static/css/", "JS_PATH":"static/js/", "IMG_PATH":"static/img/", "MOV_PATH":"static/mov/"}
 locale_dict = {"af_ZA":"Afrikaans","ar_AR":"Arabic","az_AZ":"Azerbaijani","be_BY":"Belarusian","bg_BG":"Bulgarian","bn_IN":"Bengali","bs_BA":"Bosnian","ca_ES":"Catalan","cs_CZ":"Czech","cy_GB":"Welsh","da_DK":"Danish","de_DE":"German","el_GR":"Greek","en_GB":"English (UK)","en_PI":"English (Pirate)","en_UD":"English (Upside Down)","en_US":"English (US)","eo_EO":"Esperanto","es_ES":"Spanish (Spain)","es_LA":"Spanish","et_EE":"Estonian","eu_ES":"Basque","fa_IR":"Persian","fb_LT":"Leet Speak","fi_FI":"Finnish","fo_FO":"Faroese","fr_CA":"French (Canada)","fr_FR":"French (France)","fy_NL":"Frisian","ga_IE":"Irish","gl_ES":"Galician","he_IL":"Hebrew","hi_IN":"Hindi","hr_HR":"Croatian","hu_HU":"Hungarian","hy_AM":"Armenian","id_ID":"Indonesian","is_IS":"Icelandic","it_IT":"Italian","ja_JP":"Japanese","ka_GE":"Georgian","km_KH":"Khmer","ko_KR":"Korean","ku_TR":"Kurdish","la_VA":"Latin","lt_LT":"Lithuanian","lv_LV":"Latvian","mk_MK":"Macedonian","ml_IN":"Malayalam","ms_MY":"Malay","nb_NO":"Norwegian (bokmal)","ne_NP":"Nepali","nl_NL":"Dutch","nn_NO":"Norwegian (nynorsk)","pa_IN":"Punjabi","pl_PL":"Polish","ps_AF":"Pashto","pt_BR":"Portuguese (Brazil)","pt_PT":"Portuguese (Portugal)","ro_RO":"Romanian","ru_RU":"Russian","sk_SK":"Slovak","sl_SI":"Slovenian","sq_AL":"Albanian","sr_RS":"Serbian","sv_SE":"Swedish","sw_KE":"Swahili","ta_IN":"Tamil","te_IN":"Telugu","th_TH":"Thai","tl_PH":"Filipino","tr_TR":"Turkish","uk_UA":"Ukrainian","vi_VN":"Vietnamese","zh_CN":"Simplified Chinese (China)","zh_HK":"Traditional Chinese (Hong Kong)","zh_TW":"Traditional Chinese (Taiwan)"}
 
+TYPE_EXCEPTION = 0
+TYPE_SUCCESS = 1
+TYPE_INFORMATION = 2
+
+class BlinkException(Exception):
+    pass
+
+def removeRowFromId(cur,table,id_field,id_value):
+    sql = "DELETE FROM " + table + " WHERE " + id_field + "=%s"
+    data = (id_value,)
+
+    try:
+        cur.execute(sql,data)
+    except (psycopg2.ProgrammingError,IndexError,Exception) as ex:
+        if type(ex) is psycopg2.ProgrammingError:
+            raise BlinkException('removeRow','Table already exist or not found; SQL syntax error; Wrong number of parameter;',ex,{"1":table})
+        elif type(ex) is IndexError:
+            raise BlinkException('removeRow','Cursor out of bounds.',ex,{"1":table})
+        else:
+            raise BlinkException('removeRow','Not Identified. Mysterious Error',ex,{"1":table})
+
+#Action response Package
+def ARP(pk_type,ex,user_data):
+    if pk_type==0:  #Exception
+        ex_obj = {"ex_type":ex.args[0],"ex_info":ex.args[1],"ex_obj":ex.args[2],"ex_extra":ex.args[3]}
+        p = {"type":"exception","ex_obj":ex_obj,"user_data":user_data}
+    elif pk_type==1:    #Success
+        p = {"type":"success","ex_obj":None,"user_data":user_data}
+    elif pk_type==2:    #Information
+        p = {"type":"information","ex_obj":None,"user_data":user_data}    
+
+    return json.dumps(p)
+
+def insert_user(cur,**a):
+    # conn = connect_database()
+    # cur = conn.cursor();
+
+    try:
+        sql = "SELECT id FROM users WHERE fbid=%s"    
+        cur.execute( sql,(a["fbid"],) )        
+
+        if cur.rowcount==0:
+            sql = "INSERT INTO users(name,age,city,country,location,gender,fbid,rating) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id"
+            data = (a["name"],a["age"],a["city"],a["language"],a["gps_location"],a["gender"],a["fbid"],a["rating"])
+
+            cur.execute(sql,data)
+            user_id = cur.fetchone()[0]            
+        else:
+            user_id = cur.fetchone()[0]
+    except (psycopg2.ProgrammingError,IndexError,Exception) as inst:
+        if type(inst) is psycopg2.ProgrammingError:
+            raise BlinkException('Insert_User','Table already exist or not found; SQL syntax error; Wrong number of parameter;',inst,{"1":sql % data})
+        elif type(inst) is IndexError:
+            raise BlinkException('Insert_User','Cursor is out of bounds',inst,{});
+        else:
+            raise BlinkException('Insert_User','Not Identified. Mysterious Error',inst,{})
+
+    return user_id
+
+def insert_question(cur,**a):
+    # conn = connect_database()
+    # cur = conn.cursor();
+
+    try:
+        sql = "INSERT INTO activities(name,keywords) VALUES(%s,%s) RETURNING id"
+        data = (a["question"],a["keywords"])    
+
+        cur.execute(sql,data)
+        return cur.fetchone()[0]    
+    except (psycopg2.ProgrammingError,IndexError,Exception) as inst:
+        if type(inst) is psycopg2.ProgrammingError:
+            raise BlinkException('Insert_Question','Table already exist or not found; SQL syntax error; Wrong number of parameter;',inst,{"1":sql % data})
+        elif type(inst) is IndexError:
+            raise BlinkException('Insert_Question','Cursor is out of bounds',inst,{});
+        else:
+            raise BlinkException('Insert_Question','Not Identified. Mysterious Error',inst,{})
+    
+
+def insert_request(cur,**a):
+    try:
+        sql = "INSERT INTO requests(activity_id,type,user_id,status) VALUES(%s,%s,%s,%s) RETURNING id"
+        data = (a["activity_id"],a["request_type"],a["user_id"],a["status"])
+
+        cur.execute(sql,data)
+        return cur.fetchone()[0]
+    except:
+        if type(inst) is psycopg2.ProgrammingError:
+            raise BlinkException('Insert_Request','Table already exist or not found; SQL syntax error; Wrong number of parameter;',inst,{"1":sql % data})
+        elif type(inst) is IndexError:
+            raise BlinkException('Insert_Request','Cursor is out of bounds',inst,{});
+        else:
+            raise BlinkException('Insert_Request','Not Identified. Mysterious Error',inst,{}) 
+
+def insert_international_by_code(cur,**a):
+
+    try:
+        sql_0 = "SELECT blinker_id FROM international_users WHERE blinker_id=%s"
+        data_0 = (a["blinker_id"],)
+
+        cur.execute(sql_0,data_0)
+        if cur.rowcount>0:
+            return "EXIST"
+
+        sql_1 = "UPDATE access_code SET used_by=%s, status='U' WHERE access_code=%s"
+        data_1 = (a["blinker_id"],a["user_code"])
+
+        sql_2 = "INSERT INTO international_users (blinker_id,document_1_id,document_2_id,contact_1_id,contact_2_id,status) VALUES (%s,%s,%s,%s,%s,%s)"
+        data_2 = (a["blinker_id"],a["document_1_id"],a["document_2_id"],a["contact_1_id"],a["contact_2_id"],a["status"])
+        
+        cur.execute(sql_1,data_1)
+        cur.execute(sql_2,data_2)
+
+        return "OK"
+    except (psycopg2.ProgrammingError,IndexError,Exception) as inst:
+        if type(inst) is psycopg2.ProgrammingError:
+            raise BlinkException('Insert_Request','Table already exist or not found; SQL syntax error; Wrong number of parameter;',inst,{"1":sql % data})
+        elif type(inst) is IndexError:
+            raise BlinkException('Insert_Request','Cursor is out of bounds',inst,{});
+        else:
+            raise inst
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     hd_lang = request.headers["Accept-Language"]
     hd_lang = hd_lang.split(",")[0];
+
     if request.method == 'GET':
         if hd_lang=="pt-BR":
             paths_dict["portuguese"] = True
@@ -189,67 +310,105 @@ def index():
         else:
             return render_template("teaser_en_us.html",**paths_dict)
     else:
-        name = request.form["field-name"]
-        gender = request.form["field-gender"]
-        fbid = request.form["field-fbid"]
+        try:
+            conn = connect_database()
+            cur = conn.cursor()
 
-        locale = request.form["field-locale"]
-        if locale!=None:
-            language = locale_dict[locale];
+            #User insert information processing
+            locale = request.form["field-locale"]
+            if locale!=None:
+                language = locale_dict[locale];
+
+            city = request.form["field-location"]        
+            if city is None:
+                city = "None";            
+
+            birthday_list = request.form["field-birthday"].split("/")    
+            birthday = datetime.datetime(int(birthday_list[2]),int(birthday_list[0]),int(birthday_list[1]))                       
+            age = (datetime.datetime.now() - birthday).days/365
+
+            user_id = insert_user(cur,name=request.form["field-name"],gender=request.form["field-gender"],fbid=request.form["field-fbid"],age=age,city=city,language=language,gps_location="GPS-Coordinates",rating=0.00)
+
+            if "user_code" in session:
+                ret = insert_international_by_code(cur,user_code=session["user_code"],blinker_id=user_id,document_1_id=None,document_2_id=None,contact_1_id=None,contact_2_id=None,status='A')
+                session.pop("user_code",None)
+
+            activity_id = insert_question(cur,question=request.form["field-question"],keywords="")
+            request_id = insert_request(cur,activity_id=activity_id,request_type=request.form["field-request-type"],status="A",user_id=user_id)
         
-        city = request.form["field-location"]        
-        if city is None:
-            city = "None";            
+            conn.commit()
 
-        birthday_list = request.form["field-birthday"].split("/")    
-        birthday = datetime.datetime(int(birthday_list[2]),int(birthday_list[0]),int(birthday_list[1]))                       
-        age = (datetime.datetime.now() - birthday).days/365
+            t1 = {"table":"requests","id_field":"id","id_value":request_id}
+            t2 = {"table":"activities","id_field":"id","id_value":activity_id}
+            # t3 = {"table":"users","id_field":"id","id_value":user_id}            
+            t4 = {"table":"international_users","id_field":"blinker_id","id_value":user_id}
+            l = [t1,t2,t4]
+            user_data = {"test_data":l}
+            
+            return ARP(TYPE_SUCCESS,None,user_data)
 
-        gps_location = "GPS-Coordinates"
-        rating = 0.00
+        except (BlinkException,Exception) as inst:
+            if type(inst) is BlinkException:
+                return ARP(TYPE_EXCEPTION,inst,None)  
+            else:
+                ex = {}
+                for i in range(0,len(inst.args)):
+                    ex.update( {i:inst.args[i]} )
+
+                return ARP(TYPE_EXCEPTION,BlinkException('Index','Not Identified. Mysterious Error',ex,{}),None)
+
+@app.route('/validate_code', methods=['POST'])
+def validate_code():
+
+    try:
+        code = request.form["code"]
+        query = "SELECT extra_field,status FROM access_code WHERE access_code like %s"
 
         conn = connect_database()
         cur = conn.cursor();
 
-        sql_test_user_exist = "SELECT id FROM users WHERE fbid=%s"
-        sql_test_user_exist_data = (fbid,)
-        cur.execute(sql_test_user_exist,sql_test_user_exist_data)
-        if cur.rowcount==0:
-
-            sql_user = "INSERT INTO users(name,age,city,country,location,gender,fbid,rating) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id"
-            sql_user_data = (name,age,city,language,gps_location,gender,fbid,rating)
-
-            cur.execute(sql_user,sql_user_data)
-            user_id = cur.fetchone()[0]            
+        cur.execute(query,(code,))
+        if cur.rowcount>0:
+            row = cur.fetchone()
+            if row[1]!='N':
+                return ARP(TYPE_INFORMATION,None,{"info":"USED"})
+            else:
+                session["user_code"] = code
+            return ARP(TYPE_SUCCESS,None,None)
+    except (psycopg2.ProgrammingError,IndexError,Exception) as inst:
+        if type(inst) is psycopg2.ProgrammingError:
+            ex = Exception('Validate_Code','Table already exist or not found; SQL syntax error; Wrong number of parameter;',inst,{"1":sql % data})
+        elif type(inst) is IndexError:
+            ex = Exception('Validate_Code','Cursor is out of bounds',inst,{});
         else:
-            user_id = cur.fetchone()[0]
+            ex = Exception('Validate_Code','Not Identified. Mysterious Error',inst,{})
 
-        question = request.form["field-question"]
-        keywords = ""
+        return ARP(TYPE_EXCEPTION,ex,None)
 
-        sql_activity = "INSERT INTO activities(name,keywords) VALUES(%s,%s) RETURNING id"
-        sql_activity_data = (question,keywords)
+    finally:
+        cur.close();
+        conn.close();        
 
-        request_type = request.form["field-request-type"]
-        status = "A" #Available,Waiting,Finished
-        
-        try:
-            cur.execute(sql_activity,sql_activity_data)
-            activity_id = cur.fetchone()[0]
+@app.route('/rollback', methods=['POST'])
+def rollback():
+    try:
+        conn = connect_database()
+        cur = conn.cursor()
 
-            sql_request = "INSERT INTO requests(activity_id,type,user_id,status) VALUES(%s,%s,%s,%s) RETURNING id"
-            sql_request_data = (activity_id,request_type,user_id,status)
-            cur.execute(sql_request,sql_request_data)
+        rb_obj = request.form["rollbackObj"]    #It is a list of dictionaries
+        rb_obj = json.loads(rb_obj)
 
-            conn.commit()
+        for el in rb_obj:
+            removeRowFromId(cur,el["table"],el["id_field"],el["id_value"])
 
-            return "OK"
-        except:
-            return "ERRO"
-        finally:
-            cur.close()
-            conn.close()
+        conn.commit()
+    except Exception as ex:
+        return ARP(TYPE_EXCEPTION,ex,None) 
+    finally:    
+        cur.close()
+        conn.close()
 
+    return "OK"
 
 
 @app.route('/blog', methods=['GET'])
--- static/js/teaser.js
+++ static/js/teaser.js
@@ -2,7 +2,13 @@ function teaser_init(suggestions){
 	var ip = new InteractionPanel(suggestions);
 
 	var to = new TestObject(ip);
+	var lt  = new LogicalTest(ip);
 	var debug = false;
+
+	$(".tab-code-container").click(function(){
+		ip.showCodePanel();
+	});
+
 
 	$(window).keydown(function(event){
 		// console.log(event.keyCode);
@@ -20,6 +26,12 @@ function teaser_init(suggestions){
 			ip.login();
 		}
 	});	
+
+	$("#field-code").keydown(function(e){
+		if(e.keyCode==13){
+			ip.validateCode( $(e.target).val().trim() );		
+		}
+	});		
 
 	$(".button-container button").click(function(){ip.login();});		
 	$("#btn-try-again").click(function(){ip.fn_share();});	
@@ -49,9 +61,169 @@ function TestObject(ip){
 			ip.fn_processing();
 		}else if(stage==5){
 			ip.fn_success();	
+		}else if(stage==6){
+			$(".tab-code-container").click();
+		}else if(stage==7){
+			ip.test_codeIsNotValid( {"user_data":{"type":"exception"}} );
+		}else if(stage==8){
+			ip.test_codeIsNotValid( {"user_data":{"type":"information"}} );
+		}else if(stage==9){
+			ip.test_codeError("ERRO");
+		}else if(stage==10){
+			ip.test_codeIsValid("OK");
+		}else if(stage==11){
+			ip.test_hideModal();
 		}
-		stage = (stage+1)%6;
+		stage = (stage+1)%12;
 	};
+}
+
+function LogicalTest(ip){
+	var stage = 0;
+
+	this.run = function(){	
+		if(stage==0){
+			$(".suggestions-container").click();
+		}else if(stage==1){
+			$("#field-question").val("My Activity Test");
+		}else if(stage==2){
+			$(".field-container .btn").click();			
+		}else if(stage==3){
+			ip.fn_share();
+		}else if(stage==4){
+			$(".tab-code-container").click();
+		}else if(stage==5){
+			$("#field-code").focus();
+			$("#field-code").val("Mdb69nKHMln");
+		}else if(stage==6){
+			ip.validateCode( $("#field-code").val().trim() );
+		}else if(stage==7){
+			$("#field-question").val("My Activity Test");
+		}else if(stage==8){
+			$(".field-container .btn").click();			
+		}else if(stage==9){
+			// console.log("ROLLBACK");
+			ip.rollback();
+		}
+
+		stage = (stage+1)%10;
+	};
+}
+
+function Rollback(){
+	var rollbackData = [];
+	var that = this;
+
+	var fn_success = function(r){
+
+	}
+
+	var fn_error = function(r){
+
+	}
+
+	this.addRollbackData = function(data){
+		rollbackData.push(data);
+	}
+
+	this.rollback = function(){
+		serverRequest("POST",{ "rollbackObj":JSON.stringify(rollbackData) },"rollback",fn_success,fn_error);
+	}
+
+	this.getData = function(){return rollbackData;}
+
+}
+
+
+function ValidationPanel(){
+
+	var that = this;
+
+	var curPanel = null;
+	var showMessage = function(panel,fade,afterFade_fn){
+		if(curPanel==null){
+			panel.show();
+			curPanel = panel;
+		}else{
+			showPanel(panel,fade)
+		}
+	}
+
+	var showPanel = function(panel,fade,afterFade_fn){
+		if(fade){
+			curPanel.fadeOut(function(){
+				panel.fadeIn(afterFade_fn);
+
+				curPanel = panel;
+			});
+		}else{
+			curPanel.hide();
+			panel.show();
+
+			curPanel = panel;
+		}
+	}	
+
+	this.show = function(){
+		$(".invalid-code").hide();
+		$(".used-code").hide();
+		$(".valid-code").hide();
+		$(".error-code").hide();
+		$("#myModal").modal( {"keyboard":true} );		
+	}
+
+	this.hide = function(){
+		$("#myModal").modal('hide');			
+	}
+
+	this.validate = function(code){
+		showMessage($("#myModal .loading-container"),false);
+
+		$.ajax({
+			url:"validate_code",
+			type:"POST",
+			data:{"code":code},
+			dataType:"json",
+			success:function(r){
+				if(r.type=="exception"){
+					that.codeIsNotValid(r);
+				}else if(r.type=="success"){
+					that.codeIsValid(r);
+				}else if(r.type=="information"){
+					that.codeIsNotValid("USED");
+				}
+			},
+			error:function(r){
+				that.codeError(r);
+			}
+		});
+	}
+
+	this.codeError = function(r){
+		showMessage($(".error-code"));
+		// console.log(r);
+	}
+
+	this.codeIsValid = function(r){
+
+		$(".container-initial").hide();
+		$(".tab-code-container").hide();
+		$(".container-inter-blinker").show();
+
+
+		showMessage($("#myModal .valid-code"),true,function(){
+			setTimeout(function(){$("#myModal").modal("hide");},500);
+		});
+				
+	}
+
+	this.codeIsNotValid = function(r){
+		if(r.user_data.type==="information"){
+			showMessage($(".used-code"));
+		}else{
+			showMessage($(".invalid-code"));
+		}		
+	}
 
 }
 
@@ -105,9 +277,33 @@ function SuggestionsPanel(divPanel,suggestions){
 	this.start();				
 }
 
+function serverRequest(method,formData,url,fn_success,fn_error,test_callback){
+
+	$.ajax({
+		"type":method,
+		"url":url,
+		"data": formData,
+		"dataType":"json",
+		success:function(r){
+			// console.log(r);
+			fn_success(r);			
+			if(test_callback!=null) test_callback(r);		
+		},
+		error:function(r){
+			// console.log(r);
+			fn_error(r);
+			if(test_callback!=null) test_callback(r);
+		}
+	})
+
+}
+
 function InteractionPanel(suggestions){
 	var sp1 = new SuggestionsPanel($(".fluid .suggestions-container"),suggestions);
 	var sp2 = new SuggestionsPanel($(".stacked .suggestions-container"),suggestions);
+
+	var vp = new ValidationPanel();
+	var rb = new Rollback();
 
 	var cur_container = $(".field-container");
 
@@ -120,6 +316,20 @@ function InteractionPanel(suggestions){
 	}
 
 	var that = this;
+
+	var _test_callback = function(r){
+		// console.log("TESTE",r);
+		if(r.user_data ==null)
+			return;
+		if(r.user_data.test_data==null)
+			return;
+
+		for(var i=0;i<r.user_data.test_data.length;i++){
+			rb.addRollbackData(r.user_data.test_data[i]);	
+		}	
+
+		// console.log( JSON.stringify( rb.getData() ) );
+	}
 
 	var _login = function login(){
 		if($("#field-question").val().trim().length==0){
@@ -142,7 +352,6 @@ function InteractionPanel(suggestions){
 
 	var afterLogin = function(response){
 		FB.api('/me?fields=name,age_range,locale,gender,id,location,birthday',function(response){		
-			console.log(response);
 			var formData = {
 				"field-name": response.name,
 				"field-birthday": response.birthday,
@@ -155,27 +364,16 @@ function InteractionPanel(suggestions){
 			};
 
 			// console.log(formData);
+			//method,formData,url,fn_success,fn_error,test_callback
 
-			$.ajax({
-				type:"POST",
-				url:"",
-				data: formData,
-				dataType:"text",
-				success:function(r){
-					that.fn_success(r);					
-				},
-				error:function(r){
-					that.fn_error(r);				
-				}
-			})
-
+			serverRequest("POST",formData,"",that.fn_success,that.fn_error,_test_callback)
 		});
 	}	
 
 	this.login = _login;
 
 	this.fn_error = function(r){
-		console.log(r);
+		// console.log(r);
 		cur_container.fadeOut(function(){
 			cur_container = $(".try-again-container");
 			cur_container.fadeIn();
@@ -190,7 +388,7 @@ function InteractionPanel(suggestions){
 	}
 
 	this.fn_success = function(r){
-		console.log(r);
+		// console.log(r);
 		cur_container.fadeOut(function(){
 		cur_container = $(".thanks-container");
 		cur_container.fadeIn();
@@ -221,4 +419,15 @@ function InteractionPanel(suggestions){
 		}		
 	}
 
+	this.showCodePanel = function(){vp.show();}
+	this.validateCode = function(code){vp.validate(code);}
+
+	this.test_codeIsValid = function(r){vp.codeIsValid();}
+	this.test_codeIsNotValid = function(r){vp.codeIsNotValid(r);}
+	this.test_codeError = function(r){vp.codeError(r);}
+	this.test_hideModal = function(){vp.hide();}
+
+	this.rollback = function(){
+		rb.rollback();
+	}
 }
