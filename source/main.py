eb1726f4b9aa0215ae37433c8bf34a778e7fbd98
daniel <danoan2008@gmail.com>
2013-08-31 14:17:22 -0300

    Facebook integration and template coding

--- source/main.py
+++ source/main.py
@@ -1,3 +1,5 @@
+#coding:utf-8
+
 import os
 from flask import Flask, session, request, redirect, render_template, url_for
 
@@ -8,5 +10,21 @@ app.config.from_object('conf.Config')
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+	return render_template('index.html')
+
+
+@app.route('/call_object/<call_id>')
+def call_object(call_id):
+	call_id = "http://blink-app.heroku/blog"
+	template_vars = {"call_title":"Chamado Maneiro", "call_object_id":call_id}
+
+	return render_template('call_object_template.html',**template_vars)
+
+
+@app.route('/social_panel/<call_id>')
+def social_panel(call_id):
+	call_object_id = "http://blink-app.heroku/blog"
+	template_vars = {"call_id":call_id,"call_object_id":call_object_id,"category":u"Iluminação","state":"Aguardando"}
+	
+	return render_template('social_panel_template.html',**template_vars)
 
